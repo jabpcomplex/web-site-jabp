12a755463fb5126d6c6db9c615f2b656e7ca2b72
jabpcomplex <julioacustico10@gmail.com>
2026-07-14 19:04:53 -0600

    add img carrusel y modifica ruta

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,10 +4,10 @@ import { initCarousel } from './carousel.js';
 
 // Datos (pueden moverse a JSON o API en el futuro)
 const recientes = [
-  { nombre: "Primeros pasos para usar GNU/Linux", link: "reseñas/viewer.html?file=post_1_primeros_pasos_gnu-linux.md", img: "img/carrusel/img_1.webp" },
+  { nombre: "Primeros pasos para usar GNU/Linux", link: "publicaciones_gnulinux/viewer.html?file=post_1_primeros_pasos_gnu-linux.md", img: "img/carrusel/img_1.webp" },
   { nombre: "Tutorial Python", link: "programacion/viewer.html?file=reseña_2.md", img: "img/carrusel/img_2.webp" },
   { nombre: "Análisis geoespacial", link: "investigacion/analisis_geoespacial.pdf", img: "img/carrusel/img_3.webp" },
-  { nombre: "Filosofía de la ciencia", link: "reseñas/viewer.html?file=filosofia_ciencia.md", img: "img/carrusel/img_4.webp" },
+  { nombre: "Territorios de Paz", link: "articulos_opinion/viewer.html?file=opinion_1.md", img: "img/carrusel/territorios_de_paz.webp" },
   { nombre: "Machine Learning básico", link: "programacion/viewer.html?file=ml_scikit.md", img: "img/carrusel/img_5.webp" }
 ];
 
